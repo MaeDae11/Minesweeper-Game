04f9ac73af6ff71885aa90e0656c228f03c01f6a
Katie <katherinelane11@gmail.com>
2017-08-27 19:48:07 -0400

    dynamically makes player board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,29 @@
-const printBoard = (board) => {
-    console.log('Current Board: ');
-    console.log(board[0].join(' | '))
-    console.log(board[1].join(' | '))
-    console.log(board[2].join(' | '))
-}
+// const printBoard = (board) => {
+//     console.log('Current Board: ');
+//     console.log(board[0].join(' | '))
+//     console.log(board[1].join(' | '))
+//     console.log(board[2].join(' | '))
+// }
 
-var board = [['','',''],
-['','',''],
-['','','']
-];
+// var board = [['','',''],
+// ['','',''],
+// ['','','']
+// ];
 
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+// printBoard(board);
+// board[0][1] = '1';
+// board[2][2] = 'B';
+// printBoard(board);
+
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    var board = [];
+    for (let i = 1; i < numberOfRows; i++){
+        var row = [];
+        for(let j = 0; j < numberOfColumns; j++){
+            row.push(' ');
+        } board.push(row);
+    } return board
+};
+
